d3c15053ed5e1c02917bd50e21f57bbbcfea4991
Shourya Pal <palshourya897@gmail.com>
2024-09-10 16:22:42 +0530

    fix: fix realtime showing name firstname+lastname

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -6,23 +6,24 @@ import { redirect } from "next/navigation";
 export async function POST(request: Request) {
   const clerkUser = await currentUser();
 
-  if(!clerkUser) redirect('/sign-in');
+  if (!clerkUser) redirect("/sign-in");
 
   const { id, firstName, lastName, emailAddresses, imageUrl } = clerkUser;
 
-  // Get the current user from your database
+  const safeFirstName = firstName || "";
+  const name = lastName ? `${safeFirstName} ${lastName}`.trim() : safeFirstName;
+
   const user = {
     id,
     info: {
       id,
-      name: `${firstName} ${lastName}`,
+      name,
       email: emailAddresses[0].emailAddress,
       avatar: imageUrl,
       color: getUserColor(id),
-    }
-  }
+    },
+  };
 
-  // Identify the user and return the result
   const { status, body } = await liveblocks.identifyUser(
     {
       userId: user.info.email,
